964362ca8226f9a907b090f04eb389695bcca165
Edward <duongedward44@gmail.com>
2022-07-20 13:26:42 -0500

    finished GET and DELETE

--- api/tags.js
+++ api/tags.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const { getAllTags } = require('../db');
+const { getAllTags, getPostsByTagName } = require('../db');
 
 tagsRouter.use((req, res, next) => {
     console.log("A request is being made to /tags");
   
   
     next();
+  });
+
+  tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+    const { tagName } = req.params
+    try {
+      // use our method to get posts by tag name from the db
+      const posts = await getPostsByTagName(tagName)
+      // send out an object to the client { posts: // the posts }
+      res.send({ posts })
+    } catch ({ name, message }) {
+      // forward the name and message to the error handler
+      next({ name, message });
+    }
   });
 
   tagsRouter.get('/', async (req, res) => {
